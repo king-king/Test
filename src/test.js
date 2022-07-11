557c2d463ae3c807d2747d233fbd7e5f5da892a8
wangqun28 <wangqun6@jd.com>
2022-07-11 18:07:32 +0800

    change

--- src/test.js
+++ src/test.js
@@ -1,31 +1,51 @@
 /**
- * @param {number} n
- * @return {string[]}
- * @param l 左括号已经用了几个
- * @param r 右括号已经用了几个
- * @param str 当前递归得到的拼接字符串结果
- * @param res 结果集
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
  */
-const generateParenthesis = function (n) {
-    const res = [];
-
-    function dfs(l, r, str) {
-        if (l == n && r == n) {
-            return res.push(str);
-        }
-        // l 小于 r 时不满足条件 剪枝
-        if (l < r) {
-            return;
-        }
-        // l 小于 n 时可以插入左括号，最多可以插入 n 个
-        if (l < n) {
-            dfs(l + 1, r, str + "(");
-        }
-        // r < l 时 可以插入右括号
-        if (r < l) {
-            dfs(l, r + 1, str + ")");
+var findMedianSortedArrays = function (nums1, nums2) {
+    let result = [];
+    let index1 = 0,
+        index2 = 0;
+    if (nums1.length === 0) {
+        result = nums2;
+    } else if (nums2.length === 0) {
+        result = nums1;
+    } else {
+        nums1.forEach((num, index) => {
+            if (num < nums2[index2]) {
+                index1 = index + 1;
+                result.push(num);
+            } else {
+                result.push(nums2[index2]);
+                index2 += 1;
+                for (let i = index2; i < nums2.length; i++) {
+                    if (num > nums2[i]) {
+                        result.push(nums2[i]);
+                        index2 = i;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        })
+        if (index1 < nums1.length) {
+            for (; index1 < nums1.length; index1++) {
+                result.push(nums1[index1])
+            }
+        } else if (index2 < nums2.length) {
+            for (; index2 < nums2.length; index2++) {
+                result.push(nums2[index2])
+            }
         }
     }
-    dfs(0, 0, "");
-    return res;
+    console.log(result)
+    if (result.length % 2 === 0) {
+        // 偶数
+        return (result[result.length / 2] + result[result.length / 2 - 1]) / 2
+    } else {
+        return result[(result.length - 1) / 2]
+    }
 };
+
+console.log(findMedianSortedArrays([1, 3], [2, 7]))
